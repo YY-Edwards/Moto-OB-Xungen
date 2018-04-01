1d94257e1f4184f824fc7e15039eb563eb3a6fe5
Edwards <330713927@qq.com>
2018-04-01 11:27:57 +0800

    新增myusart文件以便封装具有流控的usart接口；

---
+++ moto_xg_project/moto_xg_project/src/Application/myusart/myusart.c
@@ -0,0 +1,6 @@
+/*
+ * myusart.c
+ *
+ * Created: 2018/4/1 11:27:25
+ *  Author: Edwards
+ */ 
---
+++ moto_xg_project/moto_xg_project/src/Application/myusart/myusart.h
@@ -0,0 +1,16 @@
+/*
+ * myusart.h
+ *
+ * Created: 2018/4/1 11:27:11
+ *  Author: Edwards
+ */ 
+
+
+#ifndef MYUSART_H_
+#define MYUSART_H_
+
+
+
+
+
+#endif /* MYUSART_H_ */
